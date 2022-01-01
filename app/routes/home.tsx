84bb26484f20231d883e6cc53cb14ec4a67cad83
Zach Taylor <zachtylr04@gmail.com>
2022-01-01 14:31:44 -0700

    Create `/home` route and redirect `/` to it.

---
+++ app/routes/home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="mx-4">
+      <h1 className="my-4 text-2xl">Welcome to the Remix Time Tracker 😁</h1>
+    </div>
+  );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,5 @@
-export default function Index() {
-  return (
-    <div className="mx-4">
-      <h1 className="my-4 text-2xl">Welcome to the Remix Time Tracker 😁</h1>
-    </div>
-  );
-}
+import { redirect } from "remix";
+
+export const loader = () => {
+  return redirect("/home");
+};
