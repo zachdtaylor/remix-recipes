5badd5ee2bbd9c4d648d04a4200021752ad7be34
Zach Taylor <zachtylr04@gmail.com>
2022-01-01 14:14:23 -0700

    Add title to home page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,7 @@
 export default function Index() {
-  return <div></div>;
+  return (
+    <div className="mx-4">
+      <h1 className="my-4 text-2xl">Welcome to the Remix Time Tracker 😁</h1>
+    </div>
+  );
 }
