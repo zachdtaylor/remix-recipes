94a2b728c7fff2a2d92a5c459b9a970e3683cf8f
Zach Taylor <zachtylr04@gmail.com>
2022-01-23 21:17:51 -0700

    Add ThreeDotsIcon

--- app/components/icons.tsx
+++ app/components/icons.tsx
@@ -168,3 +168,22 @@ export function LoadingIcon() {
     </svg>
   );
 }
+
+export function ThreeDotsIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6 inline"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+      />
+    </svg>
+  );
+}
